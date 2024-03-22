c86b4bb29492e9084127f6d5a483d87c3a933021
Atul1901 <kumar.atul.20299@gmail.com>
2024-03-22 11:45:16 +0530

    code cleanup

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,6 +1,5 @@
 import DeleteAllUsers from "./DeleteAllUsers";
 import styled from "styled-components";
-// import store from "../store";
 
 const UserDetails = () => {
   return (
@@ -10,10 +9,6 @@ const UserDetails = () => {
           <div className="admin-subtitle">List of user Details</div>
           <button className="btn add-btn">Add new user</button>
         </div>
-        <ul>
-          {/* <li>hii</li>
-          <li>hiiiiii</li> */}
-        </ul>
         <hr />
         <DeleteAllUsers />
       </div>
